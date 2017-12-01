3cb6f0bb70b3694b78b7ee87e8eecefef52b16b8
almowafy <abdulrahmanalmowafy44@gmail.com>
2017-12-01 10:07:37 +0200

    initial commit

---
+++ IdentityTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityTesting.Startup))]
+namespace IdentityTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
